2449707b61f8c949dd801c4782bb4b165f7cb600
Mihai Ciucu <mihai.ciucu@gmail.com>
2017-02-11 08:31:15 +0200

    * refactor StorageMap

--- src/Stem.es6.js
+++ src/Stem.es6.js
@@ -19,7 +19,7 @@ export * from "base/Ajax";
 export * from "base/Plugin";
 export * from "base/Utils";
 export * from "base/URLRouter";
-export * from "base/Storage";
+export * from "base/StorageMap";
 export * from "time/Time";
 export * from "data-structures/Deque";
 export * from "data-structures/MultiMap";
