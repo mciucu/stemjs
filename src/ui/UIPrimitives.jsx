5b04374bbcfcadafbc2487a07ac860410e07b878
Mihai Ciucu <mihai.ciucu@gmail.com>
2017-02-28 19:58:30 +0200

    * add ViewportMeta element

--- src/ui/UIPrimitives.jsx
+++ src/ui/UIPrimitives.jsx
@@ -235,6 +235,52 @@ class RawHTML extends UI.Element {
     }
 }
 
+class ViewportMeta extends UI.Primitive("meta") {
+    getDefaultOptions() {
+        return {
+            scale: this.getDesiredScale(),
+            initialScale: 1,
+            maximumScale: 1,
+        }
+    }
+
+    getDesiredScale() {
+        const MIN_WIDTH = this.options.minDeviceWidth;
+        return (MIN_WIDTH) ? Math.min(window.screen.availWidth, MIN_WIDTH) / MIN_WIDTH : 1;
+    }
+
+    getContent() {
+        let rez = "width=device-width";
+        rez += ",initial-scale=" + this.options.scale;
+        rez += ",maximum-scale=" + this.options.scale;
+        rez += ",user-scalable=no";
+        return rez;
+    }
+
+    extraNodeAttributes(attr) {
+        attr.setAttribute("name", "viewport");
+        attr.setAttribute("content", this.getContent());
+    }
+
+    redraw() {
+        super.redraw();
+        let print = () => console.log("W", window.innerHeight, window.innerWidth, window.devicePixelRatio);
+        print();
+        setTimeout(print, 0);
+    }
+
+    maybeUpdate() {
+        const desiredScale = this.getDesiredScale();
+        if (desiredScale != this.options.scale) {
+            this.updateOptions({scale: desiredScale});
+        }
+    }
+
+    onMount() {
+        window.addEventListener("resize", () => this.maybeUpdate());
+    }
+}
+
 class TemporaryMessageArea extends UI.Primitive("span") {
     getDefaultOptions() {
         return {
@@ -517,4 +563,5 @@ export function ConstructorInitMixin(BaseClass) {
 };
 
 
-export {Link, Panel, Image, RawHTML, TimePassedSpan, TemporaryMessageArea, SlideBar, VerticalSlideBar, HorizontalSlideBar, ScrollableMixin, InfiniteScrollable};
+export {Link, Panel, Image, RawHTML, TimePassedSpan, TemporaryMessageArea, SlideBar, VerticalSlideBar,
+    HorizontalSlideBar, ScrollableMixin, InfiniteScrollable, ViewportMeta};
