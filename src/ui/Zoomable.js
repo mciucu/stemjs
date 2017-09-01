6338ac487d6961f7aff54c62c4953ebfbf1d71b9
darius98 <marian.darius98@gmail.com>
2017-09-01 19:54:41 +0300

    * More work on Zoomable: can now add multiple zoom events

--- src/ui/Zoomable.js
+++ src/ui/Zoomable.js
@@ -1,17 +1,19 @@
 import {Device} from "../base/Device";
 import {CleanupJobs} from "../base/Dispatcher";
 
+function generateZoomEvent(rawEvent, delta, unit=200) {
+    return {
+        x: Device.getEventX(rawEvent),
+        y: Device.getEventY(rawEvent),
+        zoomFactor: (delta <= 0) ? 1 - delta / unit : 1 / (1 + delta / unit),
+        rawEvent
+    };
+}
+
 class WheelZoomEventHandler {
-    constructor(uiElement, callback, unit) {
+    constructor(uiElement, callback) {
         this.eventHandler = uiElement.addNodeListener("wheel", (event) => {
-            const deltaY = event.deltaY;
-            const factor = (deltaY <= 0) ? 1 - deltaY / unit : 1 / (1 + deltaY / unit);
-            callback({
-                x: Device.getEventX(event),
-                y: Device.getEventY(event),
-                zoomFactor: factor,
-                event: event
-            });
+            callback(event, event.deltaY);
         });
     }
 
@@ -21,7 +23,7 @@ class WheelZoomEventHandler {
 }
 
 class PinchZoomEventHandler {
-    constructor(uiElement, callback, unit) {
+    constructor(uiElement, callback) {
         this.pinchActive = false;
         this.touchStartHandler = uiElement.addNodeListener("touchstart", (event) => {
             this.recalculateCentroid(event);
@@ -34,17 +36,9 @@ class PinchZoomEventHandler {
         });
         this.touchMoveHandler = uiElement.addNodeListener("touchmove", (event) => {
             if (this.pinchActive && event.touches && event.touches.length > 1) {
-                const [newCentroid, newAveregeDist] = this.calculateCentroid(event.touches);
-                const delta = this.averegeDist - newAveregeDist;
-                const factor = (delta <= 0) ? 1 - delta / unit : 1 / (1 + delta / unit);
-                callback({
-                    x: Device.getEventX(event),
-                    y: Device.getEventY(event),
-                    zoomFactor: factor,
-                    event: event
-                });
-                this.centroid = newCentroid;
-                this.averegeDist = newAveregeDist;
+                const {centroid, averageDist} = this.calculateCentroid(event.touches);
+                callback(event, this.averageDist - averageDist);
+                Object.assign(this, {centroid, averageDist});
             }
         });
     }
@@ -58,15 +52,15 @@ class PinchZoomEventHandler {
         centroid.x /= touches.length;
         centroid.y /= touches.length;
 
-        let averegeDist = 0;
+        let averageDist = 0;
         for (const touch of touches) {
             const x = Device.getEventX(touch), y = Device.getEventY(touch);
-            averegeDist += Math.sqrt(   (x - centroid.x) * (x - centroid.x) +
+            averageDist += Math.sqrt(   (x - centroid.x) * (x - centroid.x) +
                                         (y - centroid.y) * (y - centroid.y)
                                     );
         }
-        averegeDist /= touches.length;
-        return [centroid, averegeDist]
+        averageDist /= touches.length;
+        return {centroid, averageDist};
     }
 
     recalculateCentroid(event) {
@@ -76,7 +70,7 @@ class PinchZoomEventHandler {
             return;
         }
         this.pinchActive = true;
-        [this.centroid, this.averegeDist] = this.calculateCentroid(touches);
+        Object.assign(this, this.calculateCentroid(touches));
     }
 
     cleanup() {
@@ -88,6 +82,10 @@ class PinchZoomEventHandler {
 }
 
 export const Zoomable = (BaseClass) => class Zoomable extends BaseClass {
+    static EVENT_HANDLERS = [WheelZoomEventHandler, PinchZoomEventHandler];
+
+    _zoomListeners = [];
+
     getZoomLevel() {
         return this.options.zoomLevel || 1;
     }
@@ -100,30 +98,41 @@ export const Zoomable = (BaseClass) => class Zoomable extends BaseClass {
         return this.options.maxZoomLevel || 50;
     }
 
-    setZoomLevel(zoomLevel, event) {
+    setZoomLevel(zoomLevel) {
         zoomLevel = Math.max(this.getMinZoomLevel(), zoomLevel);
         zoomLevel = Math.min(this.getMaxZoomLevel(), zoomLevel);
         if (this.getZoomLevel() === zoomLevel) {
             return;
         }
         this.options.zoomLevel = zoomLevel;
-        this.redraw();
         this.dispatch("setZoomLevel", zoomLevel);
     }
 
     addZoomListener(callback, unit=200) {
-        // callback will be called with three arguments: x, y and factor
-        const wheelEventHandler = new WheelZoomEventHandler(this, callback, unit);
-        const pinchEventHandler = new PinchZoomEventHandler(this, callback, 0.5 * unit);
-
-        return this.zoomHandler = new CleanupJobs([
-            wheelEventHandler,
-            pinchEventHandler
-        ]);
+        if (this._zoomListeners.find(listener => listener.callback === callback)) {
+            console.warn("Trying to add a listener twice!", callback);
+            return;
+        }
+        let eventHandlers = [];
+        for (const EventHandlerClass of this.constructor.EVENT_HANDLERS) {
+            eventHandlers.push(new EventHandlerClass(this,
+                (event, delta) => callback(generateZoomEvent(event, delta, unit))
+            ));
+        }
+        const listener = {
+            handler: new CleanupJobs(eventHandlers),
+            callback
+        };
+        this._zoomListeners.push(listener);
+        return listener.handler;
     }
 
-    removeZoomListener() {
-        this.zoomHandler && this.zoomHandler.cleanup();
-        delete this.zoomHandler;
+    removeZoomListener(callback) {
+        const listener = this._zoomListeners.find(listener => listener.callback === callback);
+        if (!listener) {
+            return;
+        }
+        listener.handler.cleanup();
+        this._zoomListeners.splice(this._zoomListeners.indexOf(listener), 1);
     }
 };
