6d87723dc6e14d16b26681016bf924ceb1039530
Mihai Ciucu <mihai.ciucu@gmail.com>
2019-08-09 17:40:53 +0300

    * Remove some exports that shouldn't be in UI * Remove built files

--- src/ui/UI.js
+++ src/ui/UI.js
@@ -18,13 +18,11 @@ export * from "./Router";
 export * from "./Translation";
 export * from "./Switcher";
 export * from "./tabs/TabArea";
-export * from "./tabs/FlatTabArea";
 export * from "./button/Button";
 export * from "./button/ButtonStyle";
 export * from "./button/ButtonGroup";
 export * from "./CardPanel";
 export * from "./RowList";
-export * from "./collapsible/CollapsiblePanel";
 export * from "./modal/FloatingWindow";
 export * from "./modal/Modal";
 export * from "./modal/Style";
@@ -33,17 +31,9 @@ export * from "./global-container/GlobalContainer";
 export * from "./horizontal-overflow/HorizontalOverflow";
 export * from "./horizontal-overflow/Style";
 
-export * from "./section-divider/SectionDivider";
-export * from "./section-divider/TitledSectionDivider";
-export * from "./section-divider/Accordion";
-export * from "./section-divider/Style";
 export * from "./Carousel";
 export * from "./RangePanel";
 
 export * from "./table/Table";
-export * from "./table/CollapsibleTable";
-export * from "./table/SortableTable";
-
-export * from "./DateTimePicker";
 
 export * from "./style/Theme";
