cd344a40101cabfea84595abc5b489d7b05ca724
cconache <cristian@blink.network>
2019-08-12 18:40:22 +0300

    * Stop exporting StyleElement from UI

--- src/ui/UI.js
+++ src/ui/UI.js
@@ -1,7 +1,6 @@
 export * from "./UIBase";
 export * from "./Constants";
 export * from "./Utils";
-export * from "./StyleElement";
 export * from "./Style";
 export * from "./button/Button";
 export * from "./style/Theme";
