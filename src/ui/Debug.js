8579839c346c16bcc85cc185017ce1c8010e7b26
Mihai Ciucu <mihai.ciucu@gmail.com>
2020-01-13 21:27:04 +0200

    * Remove a setTimeout in debug mode

--- src/ui/Debug.js
+++ src/ui/Debug.js
@@ -1,12 +1,5 @@
 export function applyDebugFlags(element) {
     if (document.STEM_DEBUG) {
         element.node.stemElement = element;
-        setTimeout(() => {
-            if (element.node && element.node.setAttribute) {
-                if (element.constructor.name !== "UIElement") {
-                    element.node.setAttribute("_", element.constructor.name);
-                }
-            }
-        }, 0);
     }
 }
