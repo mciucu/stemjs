7dc33e1f21e7c4acc8aacd0b31387b629f243ef9
Denis <dev@dbribe.com>
2020-02-12 20:34:39 +0200

    * Added preload property to video

--- src/ui/Video.jsx
+++ src/ui/Video.jsx
@@ -24,6 +24,7 @@ Video.domAttributesMap = CreateNodeAttributesMap(UI.Element.domAttributesMap, [
     ["muted", {noValue: true}],
     ["playsinline"],
     ["poster"],
+    ["preload"],
 ]);
 
 export {Video};
