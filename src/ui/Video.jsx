6ace1f3bbd268b1f0ba9083d8b21324b3bf67909
Denis <dev@dbribe.com>
2020-02-12 19:24:59 +0200

    * Fixes

--- src/ui/Video.jsx
+++ src/ui/Video.jsx
@@ -3,6 +3,15 @@ import {CreateNodeAttributesMap} from "NodeAttributes";
 
 
 class Video extends UI.Primitive( "video") {
+    play() {
+        return this.node.play();
+    }
+
+    render() {
+        if (this.options.source) {
+            return <source src={this.options.source} type="video/mp4" />
+        }
+    }
 }
 
 Video.domAttributesMap = CreateNodeAttributesMap(UI.Element.domAttributesMap, [
