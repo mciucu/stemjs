4ac3d961d0d7997970a5f9b2dc7e6cd99f5380a9
Darius Marian <marian.darius98@gmail.com>
2020-10-15 15:13:38 +0300

    Refactor the Device file to provide the same functionality as free functions, not as static methods of a class.

--- src/base/Device.js
+++ src/base/Device.js
@@ -1,113 +1,117 @@
 // Primitive utils for wrapping browser info
-class Device {
-    static isTouchDevice() {
-        if (!this.hasOwnProperty("_isTouchDevice")) {
-            this._isTouchDevice = !!(("createTouch" in window.document) ||
-                                            (navigator.MaxTouchPoints > 0) ||
-                                            (navigator.msMaxTouchPoints > 0) ||
-                                            ("ontouchstart" in window));
-        }
-        return this._isTouchDevice;
-    }
 
-    static isMobileDevice() {
-        if (!this.hasOwnProperty("_isMobileDevice")) {
-            const mobileDevices = ["Android", "webOS", "iPad", "iPhone", "iPod", "BlackBerry", "Windows Phone"];
-            this._isMobileDevice = false;
-            for (let device of mobileDevices) {
-                if (navigator.userAgent.indexOf(device) !== -1) {
-                    this._isMobileDevice = true;
-                }
-            }
-        }
-        return this._isMobileDevice;
-    }
-
-    static isLandscape() {
-        if (window.orientation && (window.orientation === -90 || window.orientation === 90)) {
-            return true;
-        }
-        if (!this.isMobileDevice()) {
-            return window.innerWidth > window.innerHeight;
-        }
-
-        const width = (this.isMobileDevice() && window.screen?.width) ||  window.innerWidth;
-        const height = (this.isMobileDevice() && window.screen?.height) ||  window.innerHeight;
-
-        return width > height || height < 380;
-    }
-
-    static getEventTouchIdentifier(event) {
-        return Math.min(...[...event.touches].map(touch => touch.identifier));
-    }
-
-    static getEventTouch(event) {
-        const identifier = this.getEventTouchIdentifier(event);
-        return [...event.touches].find(touch => touch.identifier === identifier);
-    }
-
-    static getEventCoord(event, axis, reference="client") {
-        let coordName = reference + axis;
-        if (event[coordName]) {
-            return event[coordName];
-        }
-        if (event.touches) {
-            return this.getEventTouch(event)[coordName];
-        }
-        if (event.originalEvent) {
-            return this.getEventCoord(event.originalEvent, axis, reference);
-        }
-
-        console.warn("Couldn't find coordinates for event. Maybe wrong reference point? (client/page/screen)");
-    }
-
-    static getEventX(event, reference="client") {
-        return this.getEventCoord(event, "X", reference);
-    }
-
-    static getEventY(event, reference="client") {
-        return this.getEventCoord(event, "Y", reference);
-    }
-
-    static getBrowser() {
-        // TODO: should try to use navigator
-        if ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0) {
-            return "Opera";
-        }
-        if (typeof InstallTrigger !== 'undefined') {
-            return "Firefox";
-        }
-        if (Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0) {
-            return "Safari";
-        }
-        if (document.documentMode) {
-            return "Internet Explorer";
-        }
-        if (window.StyleMedia) {
-            return "Edge";
-        }
-        if (window.chrome && window.chrome.webstore) {
-            return "Chrome";
-        }
-        return "Unknown";
-    }
-
-    static supportsEvent(eventName) {
-        if (!this.cachedSupportedValues.has(eventName)) {
-            var element = document.createElement("div");
-            let onEventName = "on" + eventName;
-            var isSupported = (onEventName in element);
-            if (!isSupported) {
-                element.setAttribute(onEventName, "return;");
-                isSupported = typeof element[onEventName] === "function";
-            }
-            element = null;
-            this.cachedSupportedValues.set(eventName, isSupported);
-        }
-        return this.cachedSupportedValues.get(eventName);
-    }
+export function isTouchDevice() {
+    return !!(("createTouch" in window.document) ||
+        (navigator.MaxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0) ||
+        ("ontouchstart" in window));
 }
 
-Device.cachedSupportedValues = new Map();
+export function isMobileDevice() {
+    const mobileDevices = ["Android", "webOS", "iPad", "iPhone", "iPod", "BlackBerry", "Windows Phone"];
+    for (let device of mobileDevices) {
+        if (navigator.userAgent.indexOf(device) !== -1) {
+            return true;
+        }
+    }
+    return false;
+}
 
-export {Device};
+export function isLandscape() {
+    if (window.orientation && (window.orientation === -90 || window.orientation === 90)) {
+        return true;
+    }
+    if (!this.isMobileDevice()) {
+        return window.innerWidth > window.innerHeight;
+    }
+
+    const width = (this.isMobileDevice() && window.screen?.width) ||  window.innerWidth;
+    const height = (this.isMobileDevice() && window.screen?.height) ||  window.innerHeight;
+
+    return width > height || height < 380;
+}
+
+export function getEventTouchIdentifier(event) {
+    return Math.min(...[...event.touches].map(touch => touch.identifier));
+}
+
+export function getEventTouch(event) {
+    const identifier = getEventTouchIdentifier(event);
+    return [...event.touches].find(touch => touch.identifier === identifier);
+}
+
+export function getEventCoord(event, axis, reference="client") {
+    let coordName = reference + axis;
+    if (event[coordName]) {
+        return event[coordName];
+    }
+    if (event.touches) {
+        return getEventTouch(event)[coordName];
+    }
+    if (event.originalEvent) {
+        return getEventCoord(event.originalEvent, axis, reference);
+    }
+
+    console.warn("Couldn't find coordinates for event. Maybe wrong reference point? (client/page/screen)");
+}
+
+export function getEventX(event, reference="client") {
+    return getEventCoord(event, "X", reference);
+}
+
+export function getEventY(event, reference="client") {
+    return getEventCoord(event, "Y", reference);
+}
+
+export function getBrowser() {
+    // TODO: should try to use navigator
+    if ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0) {
+        return "Opera";
+    }
+    if (typeof InstallTrigger !== 'undefined') {
+        return "Firefox";
+    }
+    if (Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0) {
+        return "Safari";
+    }
+    if (document.documentMode) {
+        return "Internet Explorer";
+    }
+    if (window.StyleMedia) {
+        return "Edge";
+    }
+    if (window.chrome && window.chrome.webstore) {
+        return "Chrome";
+    }
+    return "Unknown";
+}
+
+const supportsEventCache = new Map();
+export function supportsEvent(eventName) {
+    if (!supportsEventCache.has(eventName)) {
+        var element = document.createElement("div");
+        let onEventName = "on" + eventName;
+        var isSupported = (onEventName in element);
+        if (!isSupported) {
+            element.setAttribute(onEventName, "return;");
+            isSupported = typeof element[onEventName] === "function";
+        }
+        element = null;
+        supportsEventCache.set(eventName, isSupported);
+    }
+    return supportsEventCache.get(eventName);
+}
+
+// This object is deprecated, use the functions in this file directly instead.
+export const Device = {
+    isTouchDevice,
+    isMobileDevice,
+    isLandscape,
+    getEventTouchIdentifier,
+    getEventTouch,
+    getEventCoord,
+    getEventX,
+    getEventY,
+    getBrowser,
+    supportsEvent,
+};
