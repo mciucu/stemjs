f9a6efbab7e4b0e9dd0b9a11c2963a1f1d0693d8
Mihai Ciucu <mihai.ciucu@gmail.com>
2020-11-15 10:35:02 +0200

    * Have autoredraw also accept a list of objects to watch for all instances

--- src/decorators/AutoRedraw.js
+++ src/decorators/AutoRedraw.js
@@ -7,7 +7,7 @@ class WeakMapMemo extends WeakMap {
     }
 
     get(key, generator = this.generator) {
-        let value = this.get(key);
+        let value = super.get(key);
         if (!value) {
             value = generator(key);
             this.set(key, value);
@@ -17,29 +17,26 @@ class WeakMapMemo extends WeakMap {
 }
 
 export const redrawHandlerWeakMap = new WeakMapMemo((uiElement) => {
-    if (uiElement.node) {
-        uiElement.redraw();
-    }
+    return () => uiElement.node && uiElement.redraw();
 });
 
-// Return a memo'd function that redraws this object
+// Return a memoized function that redraws this object
 export function getUniqueRedrawHandler(uiElement) {
     return redrawHandlerWeakMap.get(uiElement);
 }
 
 // Decorator that attaches an update listener on all store objects in options
 // The logic is very crude, but works in most cases
-export function autoredraw(Cls, ...args) {
-    const oldSetOptions = Cls.prototype.setOptions;
-
+export function autoredrawDecorator(Cls, ...args) {
     const listenersForElement = new WeakMapMemo((uiElement) => {
         let listenersMap = new Map();
         for (let obj of args) {
-            const handler = uiElement.attachUpdateListener(obj, getUniqueRedrawHandler(this));
-            listenersMap.set(obj, handler);
+            listenersMap.set(obj, obj); // We're not yet mounted, this is in setOptions
         }
         return listenersMap;
     });
+
+    const oldSetOptions = Cls.prototype.setOptions;
 
     // TODO: optimize to only attach after onMount
     Cls.prototype.setOptions = function setOptions(options) {
@@ -47,25 +44,36 @@ export function autoredraw(Cls, ...args) {
 
         let listenersMap = listenersForElement.get(this);
 
-        const objArray = Object.values(options || {}).filter(obj => obj instanceof StoreObject);
-        const objSet = new Set(objArray);
+        const objArray = Object.values(options || {}).filter(obj => {
+            return (obj instanceof StoreObject) && !listenersMap.has(obj);
+        });
 
-        // Remove old objects, in case they're no longer in options
-        for (let [obj, handler] of Array.from(listenersMap.entries())) {
-            if (!objSet.has(obj)) {
-                // TODO: implement this.removeCleanupJob(handler);
-                handler.remove();
-                listenersMap.remove(obj);
-            }
-        }
+        // TODO: we don't remove listeners here, just results in some extra redraws when reassigning options
 
-        // Add the new extra ones
+        // Add the new extra ones, and attach the lister if mounted
         for (let obj of objArray) {
-            if (!listenersMap.has(obj)) {
-                const handler = this.attachUpdateListener(obj, getUniqueRedrawHandler(this));
-                listenersMap.set(obj, handler);
-            }
+            const handler = this.node ? this.attachUpdateListener(obj, getUniqueRedrawHandler(this)) : obj;
+            listenersMap.set(obj, handler);
+        }
+    }
+
+    const oldOnMount = Cls.prototype.onMount;
+    Cls.prototype.onMount = function onMount() {
+        oldOnMount.call(this);
+        let listenersMap = listenersForElement.get(this);
+        for (let obj of listenersMap.keys()) {
+            const handler = this.attachUpdateListener(obj, getUniqueRedrawHandler(this));
+            listenersMap.set(obj, handler);
         }
     }
     return Cls;
+}
+
+// Can be called decorated as @autoredraw or @autoredraw(Watch1, Watch2, ...)
+export function autoredraw(...args) {
+    if (args[0]?.prototype?.attachUpdateListener) {
+        return autoredrawDecorator(args[0]);
+    } else {
+        return (Cls) => autoredrawDecorator(Cls, ...args);
+    }
 }
