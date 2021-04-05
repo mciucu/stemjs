e10a84677f332610ff05a4aad3737af2ee1a479d
Mihai Ciucu <mihai.ciucu@gmail.com>
2021-04-05 08:52:50 +0300

    More configurable input elements

--- src/ui/input/Style.js
+++ src/ui/input/Style.js
@@ -3,12 +3,13 @@ import {StyleSheet, styleRule} from "../Style";
 class InputStyle extends StyleSheet {
     @styleRule
     inputElement = {
-        backgroundColor: "#FFF",
+        fontSize: 14,  // TODO: theme input size
+        backgroundColor: this.themeProps.INPUT_BACKGROUND_COLOR || "#FFF",
+        height: this.themeProps.INPUT_ELEMENT_DEFAULT_HEIGHT,
         transition: "border-color ease-in-out .15s, box-shadow ease-in-out .15s",
         padding: "0.4em 0.54em",
         border: "1px solid " + (this.themeProps.INPUT_BORDER_COLOR || "#CCC"),
         borderRadius: this.themeProps.INPUT_BORDER_RADIUS || 4,
-        fontSize: 14,  // TODO: theme input size
         ":focus": {
             outline: "0",
             borderColor: "#66afe9",
@@ -23,7 +24,7 @@ class InputStyle extends StyleSheet {
 
     @styleRule
     select = {
-        height: "2.12em",
+        height: this.themeProps.INPUT_ELEMENT_DEFAULT_HEIGHT || "2.12em",
     };
 }
 
