333f8f76bce39f122b409f604c77474d33c7a51f
Mihai Ciucu <mihai.ciucu@gmail.com>
2021-05-20 15:53:35 +0300

    Add CSVReader functionality

--- src/base/CSV.js
+++ src/base/CSV.js
@@ -1,7 +1,9 @@
 import {FileSaver} from "./FileSaver";
+import {UNICODE_BOM_CHARACTER} from "./Utils";
 
 const DEFAULT_FILE_NAME = "data.csv";
 
+// TODO: this should be a CSV writer, right?
 export class CSVBuilder {
     constructor(columns) {
         this.setColumns(columns);
@@ -60,5 +62,114 @@ export class CSVBuilder {
     static saveFile(columns, entries, fileName=DEFAULT_FILE_NAME) {
         let builder = new this(columns);
         return builder.saveFile(entries, fileName);
+    }
+}
+
+export class CSVReader {
+    static DELIMITER_AUTO = "auto"; // TODO this needs to be implemented
+    static DELIMITER_COMMA = ","; // The only 2 supported delimiters
+    static DELIMITER_TAB = "\t";
+
+    constructor({delimiter=CSVReader.DELIMITER_COMMA, multilineInsideQuotes = true, autotrim = true, skipBom=true} = {}) {
+        this.allowQuotes = true;
+        this.delimiter = delimiter;
+        this.multilineInsideQuotes = multilineInsideQuotes;
+        this.autotrim = autotrim;
+        this.skipBom = skipBom;
+    }
+
+    // Run a state machine here
+    // The whole input must be provided for now
+    // TODO support an input stream, and returning rows to a generator
+    readInput(input, delimiter=this.delimiter) {
+        let currentIndex = 0;
+        let insideQuote = false;
+
+        let currentColumn = "";
+        let currentRow = [];
+        let rows = [];
+
+        const finishColumn = () => {
+            if (this.autotrim) {
+                currentColumn = currentColumn.trim();
+            }
+            currentRow.push(currentColumn);
+            currentColumn = "";
+        }
+
+        const finishRow = () => {
+            if (currentColumn.length === 0 && currentRow.length === 0) {
+                // This is an empty row, skipping it
+                return;
+            }
+
+            if (insideQuote) {
+                console.error("Breaking a line inside a quote, wanna enable multilineInsideQuotes?");
+                // TODO: throw an error here if enabled
+            }
+
+            insideQuote = false;
+
+            finishColumn();
+            rows.push(currentRow);
+            currentRow = [];
+        }
+
+        if (this.skipBom) {
+            if (input.charCodeAt(0) === UNICODE_BOM_CHARACTER) {
+                currentIndex++;
+            }
+        }
+
+        for (; currentIndex < input.length; currentIndex++) {
+            const currentChar = input[currentIndex];
+            const charIsCR = (currentChar === "\r");
+            const charIsLF = (currentChar === "\n");
+            const charIsQuote = (currentChar === '"');
+
+            if (charIsCR || charIsLF) {
+                if (!this.multilineInsideQuotes || !insideQuote) {
+                    finishRow();
+                    continue;
+                }
+            }
+
+            if (insideQuote) {
+                if (charIsQuote) {
+                    if (currentIndex + 1 < input.length && input[currentIndex + 1] === '"') {
+                        // Double escaped quotes
+                        currentColumn += '"';
+                        currentIndex++;
+                    } else {
+                        insideQuote = false;
+                    }
+                } else {
+                    currentColumn += currentChar;
+                }
+            } else {
+                if (currentChar === delimiter) {
+                    currentRow.push(currentColumn);
+                    currentColumn = "";
+                } else if (charIsQuote && this.allowQuotes) {
+                    if (currentColumn.length) {
+                        currentColumn += currentChar;
+                    } else {
+                        insideQuote = true;
+                    }
+                } else {
+                    currentColumn += currentChar;
+                }
+            }
+        }
+
+        finishRow(); // Flush the last row
+
+        for (const row of rows) {
+            if (row.length != rows[0].length) {
+                throw "Row detected with a different number of columns than the header";
+            }
+        }
+
+        return rows;
     }
 }
