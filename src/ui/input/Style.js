9c7a4a9cdaa62cb2319d4507984326181d75a16a
Denis <dev@dbribe.com>
2021-05-28 13:00:11 +0300

    Remove some default style values from the input class

--- src/ui/input/Style.js
+++ src/ui/input/Style.js
@@ -3,12 +3,8 @@ import {StyleSheet, styleRule} from "../Style";
 class InputStyle extends StyleSheet {
     @styleRule
     inputElement = {
-        fontSize: 14,  // TODO: theme input size
-        backgroundColor: this.themeProps.INPUT_BACKGROUND_COLOR || "#FFF",
         transition: "border-color ease-in-out .15s, box-shadow ease-in-out .15s",
         padding: "0.4em 0.54em",
-        border: "1px solid " + (this.themeProps.INPUT_BORDER_COLOR || "#CCC"),
-        borderRadius: this.themeProps.INPUT_BORDER_RADIUS || 4,
         ":focus": {
             outline: "0",
             borderColor: "#66afe9",
