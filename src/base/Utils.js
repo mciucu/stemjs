3faac464e082ef773e005c24c549c57d76873bb1
Mihai Ciucu <mihai.ciucu@gmail.com>
2022-03-30 19:54:07 +0300

    Support data- and aria- node attributes

--- src/base/Utils.js
+++ src/base/Utils.js
@@ -143,7 +143,11 @@ export function suffixNumber(value, suffix) {
 }
 
 export function capitalize(text) {
-    return text.charAt(0).toUpperCase() + text.slice(1);
+    return text && (text.charAt(0).toUpperCase() + text.slice(1));
+}
+
+export function decapitalize(text) {
+    return text && (text.charAt(0).toLowerCase() + text.slice(1));
 }
 
 export function pluralize(count, text) {
