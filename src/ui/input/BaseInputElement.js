384ecd688f8522a4ea52377b62ae1faf1d782508
Mihai Ciucu <mihai@blink.net>
2022-12-19 01:31:25 +0200

    Add BaseInputElement interface

---
+++ src/ui/input/BaseInputElement.js
@@ -0,0 +1,41 @@
+import {UI} from "../../../stem-core/src/ui/UIBase.js";
+
+export class BaseInputElement extends UI.Element {
+    getValue() {
+        return this.value;
+    }
+
+    setValue(value, dispatchChange=true) {
+        if (this.isEqual(this.value, value)) {
+            return;
+        }
+        this.value = value;
+        if (this.node) {
+            this.redraw();
+        }
+        if (dispatchChange) {
+            this.dispatchChange(value);
+        }
+    }
+
+    isEqual(valueA, valueB) {
+        return valueA === valueB;
+    }
+
+    setOptions(options) {
+        const oldInitialValue = this.options.initialValue;
+        super.setOptions(options);
+        const {initialValue} = this.options;
+        if (this.value === undefined || !this.node || !this.isEqual(initialValue, oldInitialValue)) {
+            this.setValue(initialValue);
+        }
+    }
+
+    focus() {
+        this.node.focus();
+    }
+
+    blur() {
+        this.node.blur();
+    }
+}
