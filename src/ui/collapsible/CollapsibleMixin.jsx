a6dd4e1bc4b0b4600e595b242deca2bf210e38d0
Mihai Ciucu <mihai@blink.net>
2022-12-19 17:40:03 +0200

    No-animation alternate collapsible

--- src/ui/collapsible/CollapsibleMixin.jsx
+++ src/ui/collapsible/CollapsibleMixin.jsx
@@ -4,61 +4,86 @@ import {GlobalStyle} from "../GlobalStyle";
 import {MakeIcon} from "../SimpleElements";
 import {registerStyle} from "../style/Theme.js";
 import {isFunction} from "../../base/Utils.js";
+import {BaseInputElement} from "../input/BaseInputElement.js";
+import {styleRule, StyleSheet} from "../Style.js";
 
 
-@registerStyle(CollapsibleStyle)
-export class CollapsibleControllerIcon extends UI.Element {
-    extraNodeAttributes(attr) {
-        attr.addClass(this.styleSheet.toggleIcon);
+class SimpleCollapsibleStyle extends StyleSheet {
+    @styleRule
+    container = {
+        transition: "all 0.3s ease",
+        display: "inline-block",
     }
 
+    @styleRule
+    iconCollapsed = {
+        transform: "rotate(-90deg) !important",
+    }
+
+    @styleRule
+    collapsed = {
+        // transition: "all 0.3s ease",
+        transform: "scaleY(0)",
+        maxHeight: 0,
+    }
+}
+
+
+// If value is true, it means we're collapsed
+@registerStyle(SimpleCollapsibleStyle)
+export class CollapsibleControllerInput extends BaseInputElement {
     getTarget() {
         const {target} = this.options;
         return isFunction(target) ? target() : target;
     }
 
     expand() {
+        this.setValue(false);
+
         const panel = this.getTarget();
         if (!panel) {
             return;
         }
         const {styleSheet} = this;
-        this.options.collapsed = false;
 
-        panel.removeClass(GlobalStyle.hidden);
-        panel.addClass(styleSheet.collapsing);
-        setTimeout(() => {
-            panel.removeClass(styleSheet.collapsed);
-        }, 100); // TODO take this from this.themeProps
+        panel.removeClass(styleSheet.collapsed);
 
-        this.removeClass(styleSheet.toggleIconCollapsed);
+        this.removeClass(styleSheet.iconCollapsed);
     }
 
     collapse() {
+        this.setValue(true);
+
         const panel = this.getTarget();
         if (!panel) {
             return;
         }
         const {styleSheet} = this;
-        this.options.collapsed = true;
 
-        panel.addClass(styleSheet.collapsing);
         panel.addClass(styleSheet.collapsed);
-        // TODO(@mihai): Implement a pattern for this
-        panel.addNodeListener("transitionend", () => {
-            if (this.options.collapsed) {
-                panel.addClass(GlobalStyle.hidden);
-            }
-        });
+        // // TODO(@mihai): Implement a pattern for this
+        // panel.addNodeListener("transitionend", () => {
+        //     if (this.getValue()) {
+        //         panel.addClass(GlobalStyle.hidden);
+        //     }
+        // });
 
-        this.addClass(styleSheet.toggleIconCollapsed);
+        this.addClass(styleSheet.iconCollapsed);
     }
 
     toggle() {
-        if (this.options.collapsed) {
+        if (this.getValue()) {
             this.expand();
         } else {
             this.collapse();
+        }
+    }
+
+    applyCollapsedState() {
+        if (this.getValue()) {
+            this.collapse();
+        } else {
+            this.expand();
         }
     }
 
@@ -67,6 +92,8 @@ export class CollapsibleControllerIcon extends UI.Element {
     }
 
     onMount() {
+        super.onMount();
+
         this.addClickListener(() => {
             this.toggle();
         });
