9d853231abb10911cd9b8e046e1b3f3596103857
Mihai Ciucu <mihai@blink.net>
2022-12-19 03:51:23 +0200

    Changes to collapsible icon pattern

--- src/ui/collapsible/CollapsibleMixin.jsx
+++ src/ui/collapsible/CollapsibleMixin.jsx
@@ -2,6 +2,76 @@ import {UI} from "../UIBase";
 import {CollapsibleStyle} from "./Style";
 import {GlobalStyle} from "../GlobalStyle";
 import {MakeIcon} from "../SimpleElements";
+import {registerStyle} from "../style/Theme.js";
+import {isFunction} from "../../base/Utils.js";
+
+
+@registerStyle(CollapsibleStyle)
+export class CollapsibleControllerIcon extends UI.Element {
+    extraNodeAttributes(attr) {
+        attr.addClass(this.styleSheet.toggleIcon);
+    }
+
+    getTarget() {
+        const {target} = this.options;
+        return isFunction(target) ? target() : target;
+    }
+
+    expand() {
+        const panel = this.getTarget();
+        if (!panel) {
+            return;
+        }
+        const {styleSheet} = this;
+        this.options.collapsed = false;
+
+        panel.removeClass(GlobalStyle.hidden);
+        panel.addClass(styleSheet.collapsing);
+        setTimeout(() => {
+            panel.removeClass(styleSheet.collapsed);
+        }, 100); // TODO take this from this.themeProps
+
+        this.removeClass(styleSheet.toggleIconCollapsed);
+    }
+
+    collapse() {
+        const panel = this.getTarget();
+        if (!panel) {
+            return;
+        }
+        const {styleSheet} = this;
+        this.options.collapsed = true;
+
+        panel.addClass(styleSheet.collapsing);
+        panel.addClass(styleSheet.collapsed);
+        // TODO(@mihai): Implement a pattern for this
+        panel.addNodeListener("transitionend", () => {
+            if (this.options.collapsed) {
+                panel.addClass(GlobalStyle.hidden);
+            }
+        });
+
+        this.addClass(styleSheet.toggleIconCollapsed);
+    }
+
+    toggle() {
+        if (this.options.collapsed) {
+            this.expand();
+        } else {
+            this.collapse();
+        }
+    }
+
+    render() {
+        return MakeIcon("chevron-down");
+    }
+
+    onMount() {
+        this.addClickListener(() => {
+            this.toggle();
+        });
+    }
+}
 
 function CollapsibleMixin(BaseClass, CollapsibleClass = CollapsibleStyle) {
     class CollapsibleElement extends BaseClass {
