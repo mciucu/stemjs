0978266de28d34bc57309eeb23439b935c077532
Mihai Ciucu <mihai@blink.net>
2022-12-20 18:31:05 +0200

    Update RandGen methods

--- src/numerics/Random.js
+++ src/numerics/Random.js
@@ -1,9 +1,23 @@
-// Several times faster than Math.random()
-export class RandGenFast {
-    constructor(x=Date.now() ^ Math.floor(100000 * Math.random()), y=0x123151df, z=0xcafe0dee) {
-        this.x = x | 0;
+import {isString} from "../base/Utils.js";
+
+export class RandGen {
+    constructor(seed = Date.now() ^ Math.floor(100000 * Math.random()), y = 0xf23151df, z = 0xcafe0dee) {
+        this.x = seed | 0;
         this.y = y | 0;
         this.z = z | 0;
+
+        if (isString(seed)) {
+            for (let i = 0; i < seed.length; i++) {
+                const chr = seed.charCodeAt(i);
+                this.x += chr ^ (chr << 16) ^ (chr << 24);
+                this.y += chr;
+                this.z -= chr;
+                this.nextInt();
+            }
+            for (let i = 0; i < 100; i++) {
+                this.nextInt();
+            }
+        }
     }
 
     nextInt() {
@@ -15,13 +29,30 @@ export class RandGenFast {
     }
 
     random() {
-        return this.nextInt() / 2147483648.0;
+        return (this.nextInt() >>> 0) / (2 ** 31);
     }
 
     rand(N) {
         return this.nextInt() % N;
     }
 
+    hex(count=8) {
+        let result = "";
+        while (count > 0) {
+            const charAvailable = 8;
+            const hexStr = this.nextInt().toString(16).padEnd(charAvailable, "0");
+            const charTaken = Math.min(charAvailable, count);
+            count -= charTaken;
+            result += hexStr.substring(0, charTaken);
+        }
+    }
+
+    pick(elements) {
+        const poz = this.rand(elements.length);
+        return elements[poz];
+    }
+
+    // TODO Should probably have a decorator that creates an instance and adds all static methods
     static random() {
         return this.instance.random();
     }
@@ -35,4 +66,4 @@ export class RandGenFast {
     }
 }
 
-RandGenFast.instance = new RandGenFast();
+RandGen.instance = new RandGen();
