7eb3731da60b1ec3869b038750122da73cda4345
Mihai Ciucu <mihai@blink.net>
2023-02-01 12:58:00 +0200

    Add type hints to Theme

---
+++ src/ui/style/ThemeTypes.js
@@ -0,0 +1,26 @@
+import {isString} from "../../base/Utils.js";
+
+export class ThemeType {
+    constructor(type, value, options) {
+        options = options || {};
+        if (isString(options)) {
+            options = {comment: options};
+        }
+        this.type = type;
+        this.value = value;
+        this.options = options;
+    }
+}
+
+// TODO this should also have a validator here for instance
+export function MakeThemeType(type) {
+    return (value, options={}) => new ThemeType(type, value, options);
+}
+
+export const ColorType = MakeThemeType("Color");
+export const SizeType = MakeThemeType("Size");
+export const BorderType = MakeThemeType("Border");
+export const BoxShadowType = MakeThemeType("Box Shadow");
+export const FontFamilyType = MakeThemeType("Font Family");
+export const FontWeightType = MakeThemeType("Font Weight");
+export const BoolType = MakeThemeType("Boolean");
