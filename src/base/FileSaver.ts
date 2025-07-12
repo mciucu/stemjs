eabef7cdffcb785ac66f6cbf9a23e7bf32ef7e2e
Mihai Ciucu <mihai@blink.net>
2025-07-12 20:43:26 +0300

    Typescript FileSaver

--- src/base/FileSaver.ts
+++ src/base/FileSaver.ts
@@ -1,59 +1,76 @@
 // The FileSaver class is mean to be able to create a Save as... file dialog from text/bytes
 // TODO: this file is work in progress
 import {Dispatchable} from "./Dispatcher";
-import {UNICODE_BOM_CHARACTER} from "./Utils";
+import {isSafari, UNICODE_BOM_CHARACTER} from "./Utils";
 
-let autoBom = function (blob) {
+interface FileSaverOptions {
+    autoBom?: boolean;
+}
+
+interface BlobOptions {
+    type?: string;
+}
+
+function AddAutoBom(blob: Blob): Blob {
     // Add the unicode boom if not present
     if (/^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(blob.type)) {
         return new Blob([String.fromCharCode(UNICODE_BOM_CHARACTER), blob], {type: blob.type});
     }
     return blob;
-};
+}
 
-class FileSaver extends Dispatchable {
-    constructor(blob, fileName, options={}) {
+export class FileSaver extends Dispatchable {
+    blob: Blob;
+    fileName: string;
+    options: FileSaverOptions;
+    saveLink: HTMLAnchorElement;
+    readyState: number;
+
+    static readonly INIT = 0;
+    static readonly WRITING = 1;
+    static readonly DONE = 2;
+
+    constructor(blob: Blob, fileName: string, options: FileSaverOptions = {}) {
         super();
         this.blob = blob;
         this.fileName = fileName;
         this.options = options;
 
         if (this.options.autoBom) {
-            this.blob = autoBom(this.blob);
+            this.blob = AddAutoBom(this.blob);
         }
 
         // TODO: these should be static
         this.saveLink = document.createElement("a");
-        let canUseSaveLink = "download" in this.saveLink;
-        let is_safari = /constructor/i.test(window.HTMLElement) || window.safari;
-		let is_chrome_ios =/CriOS\/[\d]+/.test(navigator.userAgent);
+        const canUseSaveLink = "download" in this.saveLink;
+		const isChromeIOS = /CriOS\/[\d]+/.test(navigator.userAgent);
 
-        let force = blob.type === "application/octet-stream";
-        let objectUrl;
+        const force = blob.type === "application/octet-stream";
+        let objectUrl: string | undefined;
 
         this.readyState = FileSaver.INIT;
         if (canUseSaveLink) {
             objectUrl = window.URL.createObjectURL(blob);
             setTimeout(() => {
-                this.saveLink.href = objectUrl;
+                this.saveLink.href = objectUrl!;
                 this.saveLink.download = this.fileName;
                 this.click();
-                this.revoke(objectUrl);
+                this.revoke(objectUrl!);
                 this.readyState = FileSaver.DONE;
             }, 0);
             return;
         }
 
-        if ((is_chrome_ios || (force && is_safari)) && window.FileReader) {
+        if ((isChromeIOS || (force && isSafari())) && window.FileReader) {
             // Safari doesn't allow downloading of blob urls
             let reader = new FileReader();
             reader.onloadend = () => {
-                let url = is_chrome_ios ? reader.result : reader.result.replace(/^data:[^;]*;/, 'data:attachment/file;');
+                let url = isChromeIOS ? reader.result as string : (reader.result as string).replace(/^data:[^;]*;/, 'data:attachment/file;');
                 let popup = window.open(url, '_blank');
                 if (!popup) {
                     window.location.href = url;
                 }
-                url = void 0; // release reference before dispatching
+                url = undefined as any; // release reference before dispatching
                 this.readyState = FileSaver.DONE;
             };
             reader.readAsDataURL(blob);
@@ -77,7 +94,7 @@ class FileSaver extends Dispatchable {
         this.revoke(objectUrl);
     }
 
-    static saveAs(blob, fileName, blobOptions={type:"text/plain;charset=utf-8"}) {
+    static saveAs(blob: Blob | string | any[], fileName: string, blobOptions: BlobOptions = {type: "text/plain;charset=utf-8"}): FileSaver {
         if (!(blob instanceof Blob)) {
             let value = blob;
             if (!Array.isArray(value)) {
@@ -85,39 +102,21 @@ class FileSaver extends Dispatchable {
             }
             blob = new Blob(value, blobOptions);
         }
-        let fileSaver = new FileSaver(blob, fileName);
-
-        return fileSaver;
+        return new FileSaver(blob, fileName);
     }
 
-    click() {
+    click(): void {
         let clickEvent = new MouseEvent("click");
         this.saveLink.dispatchEvent(clickEvent);
     }
 
-    revoke(file) {
+    revoke(file: string | File): void {
         setTimeout(() => {
             if (typeof file === "string") {
                 window.URL.revokeObjectURL(file);
             } else {
-                file.remove();
+                (file as any).remove();
             }
         }, 1000 * 40);
     }
 }
-FileSaver.readyState = FileSaver.INIT = 0;
-FileSaver.WRITING = 1;
-FileSaver.DONE = 2;
-
-if (typeof navigator !== "undefined" && navigator.msSaveOrOpenBlob) {
-    FileSaver.saveAs = function(blob, name, no_auto_bom) {
-        name = name || blob.name || "download";
-
-        if (!no_auto_bom) {
-            blob = autoBom(blob);
-        }
-        return navigator.msSaveOrOpenBlob(blob, name);
-    };
-}
-
-export {FileSaver};
