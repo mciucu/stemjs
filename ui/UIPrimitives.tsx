b7f401daa0868963d03ba2a98183a1943a3267f9
Mihai Ciucu <mihai@blink.net>
2026-08-20 17:35:47 +0300

    Deprecate Panel fully

--- ui/UIPrimitives.tsx
+++ ui/UIPrimitives.tsx
@@ -1,13 +1,4 @@
-// TODO: this file existed to hold generic classes in a period of fast prototyping, has a lot of old code
-import {UI, UIElement, UIElementOptions} from "./UIBase";
-
+// Re-exports the primitives that used to live here
 export * from "./primitives/Link";
 export * from "./primitives/IFrame";
 export * from "./primitives/Image";
-
-/** @deprecated Use UIElementOptions - title is on every element now. */
-export type PanelOptions = UIElementOptions;
-
-/** @deprecated Extend UIElement - getTitle() is on every element now. */
-export class Panel<T extends UIElementOptions = UIElementOptions> extends UIElement<T> {
-}
