99d68b6c950e79d324202c96a483fdcce8b0800a
Mihai Ciucu <mihai@blink.net>
2026-08-31 17:04:51 +0300

    Don't import UI if we don't need it

--- state/BaseEnum.ts
+++ state/BaseEnum.ts
@@ -1,5 +1,4 @@
 import {isString, isBoolean, isNumber, titleCase} from "../base/Utils";
-import type {UICleanChild} from "../ui/UIBase";
 
 export interface EnumOptions {
     value?: any;
@@ -48,8 +47,8 @@ export class BaseEnum {
         return this.getName();
     }
 
-    toUI(parent?: any): UICleanChild;
-    toUI(): UICleanChild {
+    toUI(parent?: any): string;
+    toUI(): string {
         return this.toString();
     }
 
